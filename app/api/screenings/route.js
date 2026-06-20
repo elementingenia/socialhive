b7ffd6d6db9404bcee3606c0ad2f72caa3c02b53
elementingenia <element.ingenia@gmail.com>
2026-06-20 12:26:35 +1000

    feat: include imdb_id in screenings movie select

--- app/api/screenings/route.js
+++ app/api/screenings/route.js
@@ -25,7 +25,7 @@ export async function GET(req) {
 
   const { data: events, error } = await supabaseAdmin
     .from('events')
-    .select('*, movies(id, title, poster_url, genre, plot, runtime, rating_imdb, rating_rt, streaming_au, we_own)')
+    .select('*, movies(id, title, poster_url, genre, plot, runtime, rating_imdb, rating_rt, imdb_id, streaming_au, we_own)')
     .gte('event_date', today)
     .order('event_date')
     .order('event_time')
